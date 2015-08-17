52d6240fa2776c2a9ab12b0786aa17b8e3324c3a
njuliuyun <njuliuyun@hotmail.com>
2015-08-16 22:53:34 -0400

    add group related functions

--- includes/functions.php
+++ includes/functions.php
@@ -5,14 +5,17 @@ $dbuser = 'yun';
 $dbpass = '709309';
 $appname = "Study Group";
 
+/* establish a connection to the database*/
 $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 if ($connection->connect_error) die($connection->connect_error);
 
+/* create a table in the database*/
 function createTable($name, $query) {
     queryMysql("CREATE TABLE IF NOT EXISTS $name($query)");
     echo "Table '$name' created or already exists.<br>";
 }
 
+/* query the database*/
 function queryMysql($query) {
     global $connection;
     $result = $connection->query($query);
@@ -20,6 +23,7 @@ function queryMysql($query) {
     return $result;
 }
 
+/* destroy a session*/
 function destroySession() {
     $_SESSION = array();
     if (session_id() != "" || isset($_COOKIE[session_name()])) {
@@ -28,6 +32,7 @@ function destroySession() {
     session_destroy();
 }
 
+/* sanitize a string */
 function sanitizeString($var) {
     global $connection;
     $var = strip_tags($var);
@@ -36,6 +41,14 @@ function sanitizeString($var) {
     return $connection->real_escape_string($var);
 }
 
+/* secure a password */
+function secure($pass) {
+    $salt1 = "qm&h*";
+    $salt2 = "pg!@";
+    return hash('ripemd128', "$salt1$pass$salt2");
+}
+
+/* display someone's profile */
 function showProfile($user) {
     if (file_exists("$user.jpg")) {
         echo "<img src='$user.jpg' style='float:left;'>";
@@ -48,8 +61,27 @@ function showProfile($user) {
     }    
 }
 
-function secure($pass) {
-    $salt1 = "qm&h*";
-    $salt2 = "pg!@";
-    return hash('ripemd128', "$salt1$pass$salt2");
+/* display someone's groups */
+function showGroups($user) {
+    $result = queryMysql("SELECT * FROM groups, courses WHERE user='$user'AND groups.course = courses.course");
+    $num = $result->num_rows;
+    if ($num) {        
+        echo "<ul>";
+        for ($i = 0; $i < $num; $i++) {
+            $row = $result->fetch_array(MYSQLI_ASSOC);
+            echo "<li><a href='group.php?view=" . $row['course'] . "'>" . $row['course'] . "(" . $row['coursename'] . ")" .
+            "</a></li>";
+        }
+        echo "</ul>";
+    }
+    else echo "<div>No groups yet.</div>";
+    
+}
+
+function addGroup($user, $add) {
+    $result = queryMysql("SELECT * FROM groups WHERE user='$user' AND course='$add'");
+    if (!$result->num_rows) queryMysql("INSERT INTO groups VALUES('$user', '$add')");
+}
+function removeGroup($user, $remove) {
+    queryMysql("DELETE FROM groups WHERE user='$user' AND course='$remove'");
 }
