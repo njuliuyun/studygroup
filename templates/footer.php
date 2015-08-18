2fdb1b58a6656f104a7df150146b0532a307e483
njuliuyun <njuliuyun@hotmail.com>
2015-08-18 18:25:04 -0400

    add footer

---
+++ templates/footer.php
@@ -0,0 +1,11 @@
+        </div><!--.main-->
+        </div><!--.columnR-->
+        </div><!--.main-wrapper-->
+        <?php 
+        if ($current == 'login' || $current == 'signup') {
+            echo "<footer><i>Yun Liu - &copy; " . date('Y') . "</i></footer>";
+        }
+        ?>
+        <script src='../includes/javascript.js'></script>
+    </body>
+</html>
